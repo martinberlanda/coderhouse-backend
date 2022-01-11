ec1b5ed6312faa162fd10ca30c7e1a124135afba
Martin Berlanda <martinberlanda@hotmail.com>
2022-01-10 21:34:45 -0300

    first commit

---
+++ clase.js
@@ -0,0 +1,94 @@
+import fs from 'fs'
+
+export default class Contenedor {
+
+    constructor(nombreDeArchivo) {
+        this.nombreDeArchivo = nombreDeArchivo
+    }
+
+    async #accessFile() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.nombreDeArchivo, 'utf8', (err, data) => {
+                if (err) {
+                    try {
+                        fs.writeFileSync(this.nombreDeArchivo, '[]')
+                        console.log('Archivo no encontrado, se creará uno nuevo')
+                        resolve(this.#accessFile()) // Recursividad
+                    } catch (error) {
+                        console.error(error)
+                        reject(error)
+                    }
+                } else resolve(data)
+            })
+        })
+    }
+
+    async save(objeto) {
+        try {
+            let json = JSON.parse(await this.#accessFile())
+            let id
+            if (json.length === 0) {
+                id = 1
+                json.push({ ...objeto, id: id })
+            } else {
+                id = json[json.length - 1].id + 1
+                json.push({ ...objeto, id: id })
+            }
+            fs.writeFileSync(this.nombreDeArchivo, JSON.stringify(json), error => {
+                if (error) throw new Error(error)
+            })
+            return id
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    async getById(id) {
+        try {
+            let json = JSON.parse(await this.#accessFile())
+            let producto = json.find(producto => producto.id === id)
+            if (producto) return producto
+            else return null
+        }
+        catch (error) {
+            console.error(error)
+        }
+    }
+
+    async getAll() {
+        try {
+            return JSON.parse(await this.#accessFile())
+        }
+        catch (error) {
+            console.error(error)
+        }
+    }
+
+    async deleteAll() {
+        try {
+            fs.writeFileSync(this.nombreDeArchivo, JSON.stringify([]), error => {
+                if (error) throw new Error(error)
+                console.log('Se eliminaron todos los productos')
+            })
+        }
+        catch (error) {
+            console.error(error)
+        }
+    }
+
+    async deleteById(id) {
+        try {
+            let json = JSON.parse(await this.#accessFile())
+            let index = json.findIndex(producto => producto.id === id)
+            json.splice(index, 1)
+            fs.writeFileSync(this.nombreDeArchivo, JSON.stringify(json), error => {
+                if (error) throw new Error(error)
+                console.log('Se eliminó el producto con id: ' + id)
+            })
+        } catch (error) {
+            console.error(error)
+        }
+
+    }
+
+}
---
+++ test.js
@@ -0,0 +1,21 @@
+import Contenedor from './clase.js'
+
+let contenedor = new Contenedor('productos.txt')
+
+// Ejemplo de Save
+/* const productoAGuardar = { title: "Zapatillas", price: 342.23, thumbnail: 'https://www.google.com' }
+console.log(await contenedor.save(productoAGuardar)) */
+
+//Ejemplo de deleteAll
+/* contenedor.deleteAll() */
+
+//Ejemplo de getById 
+/* console.log(await contenedor.getById(6)) */
+
+//Ejemplo de getAll
+/* console.log(await contenedor.getAll()) */
+
+//Ejemplo de deleteById
+/* console.log(await contenedor.getAll())
+await contenedor.deleteById(1)
+console.log(await contenedor.getAll()) */
