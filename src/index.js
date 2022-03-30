517136cecde842f5fa22330b961ebff673a2a93c
Martín Berlanda <martinberlanda@hotmail.com>
2022-03-29 21:19:15 -0300

    add latest dependencies and prepare to use cookies

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import bodyParser from "body-parser";
 import "dotenv/config";
 import productsRoute from "./routes/products.route.js";
 import shoppingCartRoute from "./routes/shopping-cart.route.js";
+import connectMongo from "connect-mongo";
+import cookieParser from "cookie-parser";
+import session from "express-session";
 
 import { createServer } from "http";
 import { Server } from "socket.io";
@@ -14,9 +17,24 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+const MongoStore = connectMongo.create({
+  mongoUrl: "mongodb://localhost:27017/sesiones",
+  ttl: 60,
+});
+
 /* Middlewares */
+app.use(cookieParser());
+app.use(
+  session({
+    store: MongoStore,
+    secret: "123456789!@#$%^&*()",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
 app.use(cors({ origin: "http://localhost:4000" }));
-app.use(morgan("tiny"));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(express.static("public"));
